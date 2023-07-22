dd6dd0c673368b95687e70cd1e926deecde8c8bc
Matheus Henrique Cesário do Nascimento <127871416+MatheusCesario@users.noreply.github.com>
2023-07-22 12:05:16 -0300

    novos exercícios

---
+++ exercicios/ex08/index.js
@@ -0,0 +1,6 @@
+var vel =15
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60) { //CONDIÇÃO SIMPLES!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log('Direja sempre usando o cinto de segurança')
---
+++ exercicios/ex09/index.js
@@ -0,0 +1,7 @@
+var país = 'BR'
+console.log(`Vivendo em ${país}`)
+if (país == 'BR') {
+    console.log('Você é Brasíleiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
