e20cc0dac08562ef76d6f6a7df16318b282242b3
Matheus Henrique Cesário do Nascimento <127871416+MatheusCesario@users.noreply.github.com>
2023-08-03 12:00:14 -0300

    finalizando ex12

--- exercicios/ex12/ex/script.js
+++ exercicios/ex12/ex/script.js
@@ -1,1 +1,23 @@
-alert('olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = '../img/manha.png'
+        document.body.style.background = '#dbb99e'
+    }
+    else if (hora >= 12 && hora <= 18) {
+        // boa tarde 
+        img.src = '../img/tarde.png'
+        document.body.style.background = ' #ba3204'
+    }
+    else {
+        //boa noite
+        img.src = '../img/noite.png'
+        document.body.style.background = ' #6c4e74'
+    }
+}
