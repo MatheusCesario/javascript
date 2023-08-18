4665913e166b55a33cea4b13ad06edd05755712b
Matheus Henrique Cesário do Nascimento <127871416+MatheusCesario@users.noreply.github.com>
2023-08-18 10:57:26 -0300

    segundo exercício

--- exercicios/ex14/ex02/script.js
+++ exercicios/ex14/ex02/script.js
@@ -1,1 +1,20 @@
-alert('olá')
+function gerar(){
+    var num = window.document.querySelector('input#itxtn')
+    var tab = window.document.getElementById('itabu')
+    if (num.value.length == 0){
+        window.alert('por favor, digite um número!')
+    }else{
+        var n = Number(num.value)
+        var c = 1 
+        tab.innerHTML = ''
+        while(c <=10){
+            var item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+    }
+   
