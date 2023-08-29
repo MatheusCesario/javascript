acd97afb226fec82ed2541753e50550674f151a9
Matheus Henrique Cesário do Nascimento <127871416+MatheusCesario@users.noreply.github.com>
2023-08-29 10:55:22 -0300

    exercício ex15

--- exercicios/ex15/ambiente.js
+++ exercicios/ex15/ambiente.js
@@ -1,3 +1,13 @@
-let num = [1,2,3,4,5]
+let num = [5,8,2,9,3]
 
+num.sort()
+num.push(1)
 console.log(num)
+console.log(`o vetor tem ${num.length} posições`)
+console.log(`o primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(10)
+if (pos == -1){
+    console.log('o valor não foi encontrado!')
+} else{
+    console.log(`o valor esta na posição ${pos}`)
+}
