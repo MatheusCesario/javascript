adaf19b9553cb78be734ced80ac967da05b109d7
Matheus Henrique Cesário do Nascimento <127871416+MatheusCesario@users.noreply.github.com>
2023-09-14 12:15:33 -0300

    novo exercício

--- exercicios/ex17/modelo copy/script.js
+++ exercicios/ex17/modelo copy/script.js
@@ -1,1 +1,39 @@
-alert('olá')
+let num = window.document.querySelector('input#itxtn')
+let lista = window.document.querySelector('select#lista')
+let res = window.document.querySelector('div#res')
+let valores = []
+
+function isNumeros(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else{
+        return false
+    }
+}
+
+function inLista(n, l){
+    if(l.indexOf(Number(n)) != -1 ){
+        return true
+    }else{
+        return false
+    }
+}
+
+function add(){
+    if(isNumeros(num.value) && !inLista(num.value, valores)){
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `valor ${num.value} adicionado`
+        lista.appendChild(item)
+
+    } else{
+        alert('valor invalido ou já validado')
+
+    }
+    num.focus()
+    num.value = ''
+}
+
+function fim(){
+    
+}
